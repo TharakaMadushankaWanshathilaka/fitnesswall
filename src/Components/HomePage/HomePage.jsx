5266a46aa32ada8c2e892789caed90c57d3c9ca5
Tharaka Madushanka Wanshathilaka <it21169212@my.sliit.lk>
2024-03-28 14:49:16 +0530

    commit all & push changes

---
+++ src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+function HomePage() {
+  return (
+    <Grid container xs={12} className='px-5 lg:px-36 justify-between'>
+        <Grid item xs={0} lg={2.5}>
+
+        </Grid>
+    </Grid>
+  )
+}
+
+export default HomePage
