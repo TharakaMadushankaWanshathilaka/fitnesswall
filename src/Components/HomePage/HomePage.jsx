bbafbb3db128aa477359dfd0223b5fb8995256e3
Tharaka Madushanka Wanshathilaka <it21169212@my.sliit.lk>
2024-03-28 16:18:15 +0530

    navigation panel

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,11 +1,18 @@
 import { Grid } from '@mui/material'
 import React from 'react'
+import Navigation from '../Navigation/Navigation'
 
 function HomePage() {
   return (
     <Grid container xs={12} className='px-5 lg:px-36 justify-between'>
-        <Grid item xs={0} lg={2.5}>
-
+        <Grid item xs={0} lg={2.5} className='hidden lg:block w-full relative'>
+            <Navigation/>
+        </Grid>
+        <Grid item xs={12} lg={6} className='hidden lg:block w-full relative'>
+            <p>Center</p>
+        </Grid>
+        <Grid item xs={0} lg={3} className='hidden lg:block w-full relative'>
+            <p>Right</p>
         </Grid>
     </Grid>
   )
---
+++ src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <div className='h-screen sticky top-0' >
+        <div>
+            <div>
+                <img src='https://www.flaticon.com/free-icon/cardiogram_835378?term=heart+rate&page=1&position=2&origin=search&related_id=835378' alt='heart rate' />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navigation
