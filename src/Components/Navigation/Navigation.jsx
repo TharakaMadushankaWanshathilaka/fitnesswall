5473109abf1c3d70591d212ea7b3bcf45d2a5c08
Tharaka Madushanka Wanshathilaka <it21169212@my.sliit.lk>
2024-03-29 12:56:12 +0530

    add navigation pannel

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { navigationMenu } from './NavigationMenu'
+import { useNavigate } from 'react-router-dom'
 
 function Navigation() {
+    const navigate=useNavigate();
   return (
     <div className='h-screen sticky top-0' >
         <div>
@@ -9,7 +11,14 @@ function Navigation() {
                 <img src='logo.png' alt='logo' height={50} width={50} />
             </div>
             <div className='space-y-6'>
-                {navigationMenu}
+
+                {navigationMenu.map((item)=> <div className='cursor-pointer flex space-x-3 items-center'
+                onClick={()=>item.title==="Profile"?navigate(`/profile/${5}`)
+                :navigate(item.path)} >
+                    {item.icon}
+                    <p className='text-xl'>{item.title}</p>
+
+                </div>)}
             </div>
         </div>
     </div>
